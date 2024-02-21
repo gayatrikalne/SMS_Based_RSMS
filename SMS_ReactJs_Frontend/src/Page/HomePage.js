8c422f9af9fdb66424b4bd03b0bd7f88da48c32f
AmrutaGupta <155062513+AmrutaGupta@users.noreply.github.com>
2024-02-21 11:08:42 +0530

    Add files via upload

--- SMS_ReactJs_Frontend/src/Page/HomePage.js
+++ SMS_ReactJs_Frontend/src/Page/HomePage.js
@@ -1,81 +1,94 @@
-import "./HomePage.css";
-import main from "./main.jpg"
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import main from "./main.jpg";
+import "./HomePage.css";
+import  { useState } from 'react';
+
 
 function HomePage() {
     
     const navigate = useNavigate();
     
-    const handleButtonAdminClick = () => {
-    navigate('/AdminLogin');
+    const handleButtonOrgClick = () => {
+        navigate('/AdminLogin');
     };
+    
+        const [isHovered, setIsHovered] = useState(false);
+      
+        const stopMarquee = () => {
+          setIsHovered(true);
+        };
+      
+        const startMarquee = () => {
+          setIsHovered(false);
+        };
 
-    const handleButtonEmpClick = () => {
-        navigate('/EmployeeLogin');
-    };
 
     return(
-
         <div>
             <div className="topnav">
-                <a href="#home">Home</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+                <a href="#HomePage">Home</a>
+                <a href="/Contact">Contact</a>
+                <a href="/About">About</a>
             </div>
+            <div
+      onMouseOver={stopMarquee}
+      onMouseOut={startMarquee}
+      style={{ backgroundColor: isHovered ? '#bcbcbc' : '#ccc' }}
+    >
+      <marquee direction="left" scrollamount="5">
+        WELCOME TO SMS BASED REMOTE SERVER MONITORING SYSTEM
+      </marquee>
+    </div>
 
             <div className="container p-5">
-            <div className="row">
-                <div className="col-8">
-                    <img
-                        src={main}
-                        class="img-fluid rounded-top"
-                        alt=""
-                        id="mainimg"
-                    />
-                </div>
-                <div className="col-4" id="main">
-                
-                <div className="box">
-                    <div>
-                        <button
-                            type="button"
-                            class="btn btn-light m-4"
-                            style={{width :300}}
-                        >
-                            System
-                        </button>
+                <div className="row">
+                    <div className="col-8">
+                        <img
+                            src={main}
+                            className="img-fluid rounded-top"
+                            alt=""
+                            id="mainimg"
+                        />
                     </div>
-                    
-                    <div>
-                        <button
-                            type="button"
-                            class="btn btn-light m-4"
-                            style={{width :300}}
-                            onClick={handleButtonAdminClick}
-                        >
-                            Admin
-                        </button>
-                    </div>
+                    <div className="col-4" id="main">
+                        <div className="box">
+                            <div>
+                                <button
+                                    type="button"
+                                    className="btn btn-light m-4"
+                                    style={{width: 300}}
+                                >
+                                    System
+                                </button>
+                            </div>
+                            
+                            <div>
+                                <button
+                                    type="button"
+                                    className="btn btn-light m-4"
+                                    style={{width: 300}}
+                                    onClick={handleButtonOrgClick}
+                                >
+                                    Admin
+                                </button>
+                            </div>
 
-                    <div>
-                        <button
-                            type="button"
-                            class="btn btn-light m-4"
-                            style={{width :300}}
-                            onClick={handleButtonEmpClick}
-                        >
-                            Employee
-                        </button>
+                            <div>
+                                <button
+                                    type="button"
+                                    className="btn btn-light m-4"
+                                    style={{width: 300}}
+                                >
+                                    Employee
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                    
                 </div>
             </div>
 
-            </div>
-            
         </div>
-        
     );
 }
 
